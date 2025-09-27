da5e78169c89b3978ac2380440fc7f3e34be6e52
adnes ndikumana <ndiku6241@gmail.com>
2025-09-27 09:41:36 +0000

    backend as https://forums-emails-counter-jewelry.trycloudflare.com/ in a

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@
 import type { ApiResponse, AuthResponse, PaginatedResponse, Transaction, User, Wallet, Balance, FeeEstimation } from '@/lib/types';
 import axios, { type AxiosError, type AxiosResponse } from 'axios';
 
-const BACKEND_URL = 'https://ricky-are-mit-edge.trycloudflare.com/';
+const BACKEND_URL = 'https://forums-emails-counter-jewelry.trycloudflare.com/';
 
 // Main instance for authenticated requests
 const axiosInstance = axios.create({

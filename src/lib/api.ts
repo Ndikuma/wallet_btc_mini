0c97b6048760b159b642425215cf11fa969b354d
adnes ndikumana <ndiku6241@gmail.com>
2025-09-27 14:18:47 +0000

    add backend url as https://targeted-gave-spring-watershed.trycloudflare.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@
 import type { ApiResponse, AuthResponse, PaginatedResponse, Transaction, User, Wallet, Balance, FeeEstimation } from '@/lib/types';
 import axios, { type AxiosError, type AxiosResponse } from 'axios';
 
-const BACKEND_URL = 'https://atlas-thy-adaptation-thus.trycloudflare.com/';
+const BACKEND_URL = 'https://targeted-gave-spring-watershed.trycloudflare.com/';
 
 // Main instance for authenticated requests
 const axiosInstance = axios.create({

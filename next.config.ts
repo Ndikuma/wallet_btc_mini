383506dab491c892bbc9e6762c5aa3942472a887
adnes ndikumana <ndiku6241@gmail.com>
2025-10-02 12:17:09 +0000

    add this https://featuring-shipped-pastor-amenitiesfor images.trycloudfl

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'featuring-shipped-pastor-amenities.trycloudflare.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };

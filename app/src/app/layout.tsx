ef3596bb3a7bd05377616bcc6dc992d612ea3918
adnes ndikumana <ndiku6241@gmail.com>
2025-10-06 17:25:02 +0000

    Try fixing this error: `Runtime Error: PageNotFoundError: Cannot find mo

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter, Source_Code_Pro } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { ThemeProvider } from '@/context/theme-provider';
 import { SettingsProvider } from '@/context/settings-context';
+import { WalletProvider } from '@/context/wallet-context';
 
 const fontBody = Inter({
   subsets: ['latin'],
@@ -34,10 +35,12 @@ export default function RootLayout({
     <html lang="fr" suppressHydrationWarning>
       <body className={cn("font-body antialiased", fontBody.variable, fontCode.variable)}>
         <SettingsProvider>
-          <ThemeProvider>
-            {children}
-            <Toaster />
-          </ThemeProvider>
+          <WalletProvider>
+            <ThemeProvider>
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </WalletProvider>
         </SettingsProvider>
       </body>
     </html>

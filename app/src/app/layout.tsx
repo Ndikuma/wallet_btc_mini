ff7ab6bd05ad91e362c122321b78a5653b90bba6
adnes ndikumana <ndiku6241@gmail.com>
2025-10-06 17:22:14 +0000

    fixed this error now PageNotFoundError: Cannot find module for page: rou

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -36,8 +36,10 @@ export default function RootLayout({
       <body className={cn("font-body antialiased", fontBody.variable, fontCode.variable)}>
         <SettingsProvider>
           <ThemeProvider>
+            <WalletProvider>
               {children}
               <Toaster />
+            </WalletProvider>
           </ThemeProvider>
         </SettingsProvider>
       </body>

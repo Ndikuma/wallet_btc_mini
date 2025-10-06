48f01dad39dee353438d34861beccf18683a2ce9
adnes ndikumana <ndiku6241@gmail.com>
2025-10-06 17:13:58 +0000

    Runtime Error
    PageNotFoundError: Cannot find module for page: route no

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,48 @@
+
+import type { Metadata } from 'next';
+import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
+import { Inter, Source_Code_Pro } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/context/theme-provider';
+import { SettingsProvider } from '@/context/settings-context';
+import { WalletProvider } from '@/context/wallet-context';
+
+const fontBody = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-body',
+});
+
+const fontCode = Source_Code_Pro({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-code',
+});
+
+
+export const metadata: Metadata = {
+  title: 'Umuhora Tech Wallet',
+  description: 'Un portefeuille Bitcoin simple et non-custodial',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr" suppressHydrationWarning>
+      <body className={cn("font-body antialiased", fontBody.variable, fontCode.variable)}>
+        <SettingsProvider>
+          <ThemeProvider>
+            <WalletProvider>
+              {children}
+              <Toaster />
+            </WalletProvider>
+          </ThemeProvider>
+        </SettingsProvider>
+      </body>
+    </html>
+  );
+}
